a2b28528cf36294320dbf37b67289d3672ca7662
brad-techguy <techguyinfo@gmail.com>
2013-12-28 14:59:56 -0500

    added index and zipcode scripts

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
-/*
 var http = require('http');
 
-http.createServer(function(req,res){
-	res.writeHead(200,{'Content-type':'text/plain'});
-	res.end('Hello!');
-}).listen(1337,'127.0.0.1');
-console.log('Webserver has started');
-*/
+function start(){
+	function onRequest(req,res){
+		res.writeHead(200,{"Content-Type": "text/plain"});
+		res.write('Hello World');
+		res.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log('Server Has Started');
+}
 
-var http = require('http');
-var fs = require('fs');
-
-http.createServer(function(req,res){
-	fs.readFile('index.html',function(err,data){
-		res.writeHead(200,{'Content-type':'text/html'});
-		res.end(data);
-	});
-}).listen(1337,'127.0.0.1');
-console.log('Webserver has started');
+exports.start = start;
 
